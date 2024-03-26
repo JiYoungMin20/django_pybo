934ff209dd6c5ad16355dc2f3860fe21bdda67a3
jiyoungmin20 <ymji6922@gmail.com>
2024-03-26 12:20:10 +0900

    DEBUG False 1

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,5 +3,8 @@ from .base import *
 # 서비스용 AWS 고정 IP 주소
 ALLOWED_HOSTS = ['43.200.48.35']
 # css, js, image 파일등 static 디렉토리 위치 지정
-STATIC_ROOT = BASE_DIR / 'static/'
+# STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'pybo/static/'
 STATICFILES_DIRS = []
+# DEBUG를 False로 설정하기
+DEBUG = False 
