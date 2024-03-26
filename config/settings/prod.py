b7063320bcc90ae03b16ac555ce39402a5c2d2f7
jiyoungmin20 <ymji6922@gmail.com>
2024-03-26 14:42:55 +0900

    PostgreSQL

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,3 +8,14 @@ STATIC_ROOT = BASE_DIR / 'pybo/static/'
 STATICFILES_DIRS = []
 # DEBUG를 False로 설정하기
 DEBUG = False 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '}.nER7DQK?l:RYGT~5ha;4Jq,As?-Cwh',
+        'HOST': 'ls-d2b48d1fbffd300a1820eb8add1208caf309a059.c7wiak6ag4yg.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
